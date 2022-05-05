9de89738765abfe805896de8a491b1b28cc7601c
hiagogouveia <hiago.gouveia@gmail.com>
2022-05-05 13:16:55 -0400

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,5 +3,22 @@ def jogar():
     print("Bem vindo ao jogo da froca")
     print("********************************")
 
+    palava_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palava_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Econtrei a letra {} na posição {}".format(chute, index))
+            index = index + 1
+        print("continue jogando")
+
+
 if(__name__ == "__main__"):
     jogar()
