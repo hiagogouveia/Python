58a29547792850b389988cbc1190eb11cc0f68f8
hiagogouveia <hiago.gouveia@gmail.com>
2022-05-06 13:23:35 -0400

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,22 +3,40 @@ def jogar():
     print("Bem vindo ao jogo da froca")
     print("********************************")
 
-    palava_secreta = "banana"
+    palava_secreta = "mamao".upper()
+    letras_acertadas = ["_" for palavra in palava_secreta]
     enforcou = False
     acertou = False
+    erros = 0
 
-    while(not enforcou and not acertou):
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):
 
         chute = input("Qual a letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palava_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Econtrei a letra {} na posição {}".format(chute, index))
-            index = index + 1
-        print("continue jogando")
+        if(chute in palava_secreta):
+            index = 0
+            for letra in palava_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+            print(letras_acertadas)
+            letras_faltando = str(letras_acertadas.count('_'))
+            print('Ainda faltam acertar {} letras'.format(letras_faltando))
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+    print("Fim do jogo")
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     jogar()
+
